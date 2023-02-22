f1214b53780424200fd9860d74a63856a71ca98f
Luis Fernando Machado <>
2023-02-22 11:36:22 -0300

    first commit

---
+++ app/src/main/java/com/codinginflow/sqliterecyclerview/MainActivity.java
@@ -0,0 +1,14 @@
+package com.codinginflow.sqliterecyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
